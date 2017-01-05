1759cc8a1b58942fc5ebadd7db6c0d3acd2683e9
Ancor Santos Pérez <anckorweb@gmail.com>
2017-01-05 21:32:18 +0000

    Se han creado los archivos bases de la librería.

---
+++ example.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <title>Magic Antispam (Ancor Santos)</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Only copy this code in your header. -->
+    <script src="js/magic-antispam.js" charset="utf-8"></script>
+
+</head>
+
+<body>
+
+    <main>
+
+        <h1>Magic Antispam</h1>
+        <p></p>
+
+    </main>
+
+</body>
+</html>
