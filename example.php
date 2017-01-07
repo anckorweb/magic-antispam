f724c7723b2e32b6654b7910824b30e669fa0662
Ancor Santos Pérez <anckorweb@gmail.com>
2017-01-07 17:44:20 +0000

    Se ha finalizado la página de ejemplo. Se ha agregado un botón de "Donativo".

--- example.php
+++ example.php
@@ -1,8 +1,8 @@
 <?php
-/* (2) Form validation with PHP and MagicInput (second part) */
+/* (3) Form validation with PHP and MagicInput (second part) */
 if(strip_tags($_POST['magicInput']) == "" AND $_SERVER['REQUEST_METHOD'] == "POST")
 {
-    echo "Ok"; /* Ok! It's not spam */
+    /* Ok! It's not spam */
 }
 ?>
 
@@ -16,18 +16,67 @@ if(strip_tags($_POST['magicInput']) == "" AND $_SERVER['REQUEST_METHOD'] == "POS
 
     <link rel="stylesheet" href="css/style.css">
 
+    <style>
+    /* (2) Hide the input */
+    .magicInput{display:none;}
+    </style>
+
 </head>
 
 <body>
 
     <main>
 
-        <h1>Magic Antispam</h1>
-        <p>Created by Ancor Santos</p>
+        <header>
 
-        <p>Magic Antispam es una librería de Javascript que te permite bloquear los ataques masivos de inyección sql (SPAM) en tus formularios web.
-        Al utilizar los captcha normales en tu formulario, estás rompiendo la línea de diseño y haciendo que el usuario tenga que rellenar un campo innecesario.
-        Solo pesa 1kb, no necesita</p>
+            <img src="image/padlock.png" id="logo"/>
+            <h1 class="left">Magic Antispam</h1>
+            <p class="credits left">Created by Ancor Santos</p>
+
+        </header>
+
+        <div class="right">
+
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="LBWHBGDKK7D2L">
+            <input type="image" src="https://www.paypalobjects.com/es_ES/ES/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal. La forma rápida y segura de pagar en Internet.">
+            <img alt="" border="0" src="https://www.paypalobjects.com/es_ES/i/scr/pixel.gif" width="1" height="1">
+            </form>
+
+        </div>
+
+        <div class="clear"></div>
+
+        <p>Magic Antispam is a Javascript library that allows you to block massive sql (SPAM) injection attacks in your web forms.
+        By using the normal captcha in your form, you are breaking the design line and causing the user to fill an unnecessary field.
+
+            <ul>
+
+                <li>Block spam without using Captchas.</li>
+                <li>It does not need external libraries like jQuery, Mootools...</li>
+                <li>Easy implementation, just a few lines of code.</li>
+
+            </ul>
+
+        </p>
+
+        <div id="instruction">
+
+            <h2>Instructions for use</h2>
+
+            <ol>
+
+                <li>Insert the library in the footer of your page: <strong style="color:#3161cd;"> <?php echo htmlentities('<script src="js/magic-antispam.js" charset="utf-8" type="text/javascript"></script>'); ?></strong></li>
+                <li>Copy the <strong>.magicInput</strong> class in your css.</li>
+                <li>Validate with PHP the submission of the form. If the "magicInput" field is empty, then everything is fine: <strong>if(strip_tags($_POST['magicInput']) == "")</strong></li>
+                <li>That is all!</li>
+
+            </ol>
+
+        </div>
+
+        <h2>In action</h2>
 
         <form action="" method="post" class="magic-antispam">
 
