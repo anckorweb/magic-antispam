1f39141264807c908366e4238a38b6f9d808511b
Ancor Santos Pérez <anckorweb@gmail.com>
2017-01-07 07:18:55 +0000

    Se ha creado la parte básica de la librería. Ahora la opertiva es totalmente funcional.

--- example.php
+++ example.php
@@ -1,3 +1,11 @@
+<?php
+/* (2) Form validation with PHP and MagicInput (second part) */
+if(strip_tags($_POST['magicInput']) == "" AND $_SERVER['REQUEST_METHOD'] == "POST")
+{
+    echo "Ok"; /* Ok! It's not spam */
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -8,9 +16,6 @@
 
     <link rel="stylesheet" href="css/style.css">
 
-    <!-- Only copy this code in your header. -->
-    <script src="js/magic-antispam.js" charset="utf-8"></script>
-
 </head>
 
 <body>
@@ -18,9 +23,25 @@
     <main>
 
         <h1>Magic Antispam</h1>
-        <p></p>
+        <p>Created by Ancor Santos</p>
+
+        <p>Magic Antispam es una librería de Javascript que te permite bloquear los ataques masivos de inyección sql (SPAM) en tus formularios web.
+        Al utilizar los captcha normales en tu formulario, estás rompiendo la línea de diseño y haciendo que el usuario tenga que rellenar un campo innecesario.
+        Solo pesa 1kb, no necesita</p>
+
+        <form action="" method="post" class="magic-antispam">
+
+            <input type="text" name="name" placeholder="Name"/>
+            <input type="text" name="subname" placeholder="Surname"/>
+            <textarea placeholder="Your question"></textarea>
+            <input type="submit" value="Send"/>
+
+        </form>
 
     </main>
+
+    <!-- (1) Import the magic-antispam library -->
+    <script src="js/magic-antispam.js" charset="utf-8" type="text/javascript"></script>
 
 </body>
 </html>
--- js/magic-antispam.js
+++ js/magic-antispam.js
@@ -1,1 +1,6 @@
-
+var input = document.createElement("input");
+input.setAttribute("type", "text");
+input.setAttribute("name", "magic-antispam");
+input.setAttribute("value", "");
+input.setAttribute("class", "magicInput");
+document.getElementsByClassName('magic-antispam')[0].appendChild(input);
